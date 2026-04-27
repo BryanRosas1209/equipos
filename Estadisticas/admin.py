b5d5e038f357d8180a8c174f41ff4462a8054c99
BryanRosas1209 <bryan.rosaaas@gmail.com>
2026-04-27 15:45:36 -0700

    Si se puede

--- Estadisticas/admin.py
+++ Estadisticas/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Equipo, Jugador, Trofeo
 
-admin.site.register(Equipo)
-admin.site.register(Jugador)
-admin.site.register(Trofeo)
+@admin.register(Equipo)
+class EquipoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'ciudad') # Columnas que verás en la lista
+    search_fields = ('nombre',)        # Barra de búsqueda
+
+@admin.register(Jugador)
+class JugadorAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'posicion', 'equipo', 'goles')
+    list_filter = ('equipo', 'posicion') # Filtros laterales
+
+@admin.register(Trofeo)
+class TrofeoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'anio')
--- Estadisticas/models.py
+++ Estadisticas/models.py
@@ -1,27 +1,22 @@
 from django.db import models
 
 class Trofeo(models.Model):
-    nombre = models.CharField(max_length=100)
-    anio = models.IntegerField(help_text="Anio en que se obtuvo") # Cambiado Año -> Anio
-
-    def __str__(self):
-        return self.nombre
+    # ... tus campos ...
+    class Meta:
+        verbose_name = "Trofeo"
+        verbose_name_plural = "Trofeos"
 
 class Equipo(models.Model):
-    nombre = models.CharField(max_length=100)
-    ciudad = models.CharField(max_length=100)
-    escudo = models.ImageField(upload_to='escudos/', null=True, blank=True)
-    trofeos = models.ManyToManyField(Trofeo, related_name='equipos', blank=True)
-
-    def __str__(self):
-        return self.nombre
+    # ... tus campos ...
+    class Meta:
+        verbose_name = "Equipo"
+        verbose_name_plural = "Equipos"
 
 class Jugador(models.Model):
-    nombre = models.CharField(max_length=100)
-    foto = models.ImageField(upload_to='jugadores/', null=True, blank=True)
-    posicion = models.CharField(max_length=50)
-    goles = models.PositiveIntegerField(default=0)
-    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE, related_name='jugadores')
+    # ... tus campos ...
+    class Meta:
+        verbose_name = "Jugador"
+        verbose_name_plural = "Jugadores"
 
     def __str__(self):
         return self.nombre
